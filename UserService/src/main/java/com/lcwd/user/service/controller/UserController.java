ca8d2d0280df01c11deecc0b4d3bb1fdb015f9b5
Vishnudas Mankar <mankarvishnudas@gmail.com>
2025-07-22 20:53:56 +0530

    final project

--- UserService/src/main/java/com/lcwd/user/service/controller/UserController.java
+++ UserService/src/main/java/com/lcwd/user/service/controller/UserController.java
@@ -2,12 +2,16 @@ package com.lcwd.user.service.controller;
 
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/users")
@@ -15,6 +19,7 @@ public class UserController {
 
     @Autowired
     public UserService userService;
+
 
     // Add methods to handle user-related requests here
     @GetMapping("/")
@@ -24,11 +29,13 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
     }
 
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     @GetMapping("/{userId}")
+    @RateLimiter(name = "myLimiter", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUserById(@PathVariable String userId) {
         return ResponseEntity.ok(userService.getUser(userId));
     }
@@ -37,5 +44,11 @@ public class UserController {
     public ResponseEntity<User> updateUser(@PathVariable String userId, @RequestBody User user) {
         return ResponseEntity.ok(userService.updateUser(userId, user));
     }
+
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+        System.out.println("Fallback is executed because services is down : " + ex.getMessage());
+        return new ResponseEntity<User>(User.builder().userId("101").name("Vishnu").email("dummy@gmail.com").about("fallback response").build(), HttpStatus.OK);
+    }
+
 
 }
--- UserService/src/main/java/com/lcwd/user/service/entities/User.java
+++ UserService/src/main/java/com/lcwd/user/service/entities/User.java
@@ -1,9 +1,6 @@
 package com.lcwd.user.service.entities;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -14,6 +11,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
 
     @Id
